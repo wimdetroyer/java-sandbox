4c684e9e06d98264e361470cae295dc69efa8c0a
wdetroya <wim.detroyer@minfin.fed.be>
2024-08-21 15:49:49 +0200

    latches

--- src/main/java/be/wimdetroyer/javasandbox/jcip/ImprovedList.java
+++ src/main/java/be/wimdetroyer/javasandbox/jcip/ImprovedList.java
@@ -2,33 +2,9 @@ package be.wimdetroyer.javasandbox.jcip;
 
 import java.util.*;
 
-public class ImprovedList<T> implements List<T> {
-
-    private final List<T> underlyingList;
-
-    public ImprovedList(List<T> underlyingList) {
-        this.underlyingList = underlyingList;
-    }
-
-    // No need to use the intrinsic lock of the underlyingList, because all other methods of the underlyingList
-    // Are overridden here: it means all operations pass thru here anyway so there is no possible thread safety conflict issue at play.
-    // In contrast to ListHelper
-    public synchronized void putIfAbsent(T elem) {
-        if(!underlyingList.contains(elem)) {
-            underlyingList.add(elem);
-        }
-    }
-
-    // Delegate to other methods
-    @Override
-    public int size() {
-        return underlyingList.size();
-    }
-
-}
 
 // NOT THREAD SAFE!!
- class ListHelper<E> {
+class ListHelper<E> {
     public final List<E> list =
             Collections.synchronizedList(new ArrayList<E>());
 
@@ -52,3 +28,139 @@ public class ImprovedList<T> implements List<T> {
         }
     }
 }
+
+public class ImprovedList<T> implements List<T> {
+
+    private final List<T> underlyingList;
+
+    public ImprovedList(List<T> underlyingList) {
+        this.underlyingList = underlyingList;
+    }
+
+    // No need to use the intrinsic lock of the underlyingList, because all other methods of the underlyingList
+    // Are overridden here: it means all operations pass thru here anyway so there is no possible thread safety conflict issue at play.
+    // In contrast to ListHelper
+    public synchronized void putIfAbsent(T elem) {
+        if(!underlyingList.contains(elem)) {
+            underlyingList.add(elem);
+        }
+    }
+
+    // Delegate to other methods
+    @Override
+    public int size() {
+        return underlyingList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return List.of();
+    }
+
+}
+
---
+++ src/main/java/be/wimdetroyer/javasandbox/jcip/latch/CountDownLatchExample.java
@@ -0,0 +1,44 @@
+package be.wimdetroyer.javasandbox.jcip.latch;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CountDownLatchExample {
+
+
+    public static final int THREAD_COUNT = 10000;
+
+    public static void main(String[] args) {
+        runWith100();
+    }
+
+    @SneakyThrows
+    public static void runWith100() {
+        final CountDownLatch threadsAllDoneExecutingLatch = new CountDownLatch(THREAD_COUNT);
+        final CountDownLatch unblockExecutionLatch = new CountDownLatch(1);
+        final AtomicInteger counter = new AtomicInteger(0);
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            var thread = new Thread(new Runnable() {
+                @SneakyThrows
+                @Override
+                public void run() {
+                    System.out.println("I am thread " + Thread.currentThread().getName() + "and i am waiting to go");
+                    unblockExecutionLatch.await();
+                    System.out.println("Executing some garbage in the context of thread " + Thread.currentThread().getName());
+                    threadsAllDoneExecutingLatch.countDown();
+                    counter.incrementAndGet();
+                }
+            });
+                    thread.start();
+        }
+
+        System.out.println("Set up the execution with a 100 threads");
+        unblockExecutionLatch.countDown();
+        System.out.println("Waiting for threads to finish");
+        threadsAllDoneExecutingLatch.await();
+        System.out.println("All threads finished" + counter.get());
+    }
+
+}
